88682ed452fa1cbc87e0b2520b985341942c3904
aeipyuan <1432977406@qq.com>
2020-05-24 13:37:46 +0800

    socket配置修改

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ import 'element-ui/lib/theme-chalk/index.css';
 Vue.config.productionTip = false
 Vue.use(ElementUI)
 /* 连接socket 服务 */
-Vue.prototype.$socket = io.connect("http://172.22.25.69:80");
+Vue.prototype.$socket = io.connect("http://121.89.169.217/");
 new Vue({
   render: h => h(App),
 }).$mount('#app')
